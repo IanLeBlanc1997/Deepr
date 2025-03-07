918217a806150b6ffbcc3740da8c64b064e2e1ae
IanLeBlanc1997 <ianleblanc7667@yahoo.com>
2025-03-06 23:00:41 -0500

    first notes

---
+++ main.py
@@ -0,0 +1,4 @@
+#need to make database to keep track of all user information. A simple JSON file would probably do the trick, but a SQL database would probably scale better?
+
+thesis = input('What is your thesis?\n')
+assets = []
